8f09e2703e14e714a98510362cdcf3a35c4b197e
Dmitry Kaveshnikov <kawwa@me.com>
2021-11-10 11:46:41 +0300

    First Push

---
+++ SBBOL TEST YO/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SBBOL TEST YO
+//
+//  Created by Dmitry Kaveshnikov on 11/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
